823bfc25eec38e6d1641da523ce9e7eac39e2fec
Sijisu <sustek0302@gmail.com>
2017-04-14 13:33:52 +0200

    Remove unnecessary scripts

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <title>BakalariDown</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <?php
   require('config.php');
   $db = dbconnect();
@@ -22,8 +20,8 @@
    ?>
   <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart','table']});
-      google.charts.setOnLoadCallback(startChars);
-      function startChars() {
+      google.charts.setOnLoadCallback(startCharts);
+      function startCharts() {
         drawChart();
         drawTable();
       }
